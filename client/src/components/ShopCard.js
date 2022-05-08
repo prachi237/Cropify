20798d1ec3a5b38f87756f2125ca9c8ad5d29777
Hemant Bajaj <41hemu38@gmail.com>
2022-05-08 05:31:28 +0530

    feat: add Shop section

---
+++ client/src/components/ShopCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/ShopCard.css";
+
+const ShopCard = () => {
+  return (
+    <div className="shopCardContainer">
+      <div className="ShopCard">
+        <h1>ShopCard</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ShopCard;
